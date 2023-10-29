8f24df0fc053d2c5d1b7a54b93057493774f5e5c
sutonium87 <sutocsaba@yahoo.com>
2023-10-29 22:43:57 +0200

    upload-2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+// Import React library to work with React components
 import React from 'react';
+
+// Import ReactDOM for rendering React components in the browser
 import ReactDOM from 'react-dom/client';
+
+// Import the main application component 'App'
 import App from 'components/App';
+
+// Import the CSS styling for the application
 import './index.css';
 
+// Create a root instance using ReactDOM and render the app to the element with the 'root' id
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Use React.StrictMode for development mode checks and optimizations
   <React.StrictMode>
+    {/* Render the main App component, which serves as the root of the application */}
     <App />
   </React.StrictMode>
 );
